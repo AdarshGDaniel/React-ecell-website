672324a4af134aca0fde060dd00d0aa1fd3d0c90
adarshgdaniel <adarshgeoffrey@karunya.edu.in>
2022-06-15 13:55:54 +0530

    V1

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Getting Started with ECell Web App
+# Getting Started with KHacks Web App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-Live Demo: [Link](https://adarshgdaniel.github.io/React-ecell-website).
+Live Demo: [Link](https://adarshgdaniel.github.io/React-khacks-website/).
 
 ## Clone the Repo
 
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -26,23 +26,23 @@ function About(props) {
             <div className="container">
                 <div className="container-fluid mar-top-bot-50" data-aos="fade-up" data-aos-delay="100">
                     <h4>Who are we?</h4>
-                    <p>A pack of enthusiastic and persevering students working towards promoting and inducing the Entrepreneurial Mindset among the students in
+                    <p>A pack of enthusiastic and persevering students working towards promoting and inducing the Entrepreneurial Mindset among the students at
                         <br /><b>Karunya Institute of Technology and Sciences</b></p>
                     <a href="https://karunya.edu" target="_blank"><i>View more about Karunya<IoIosLink className="link-hover" /></i></a>
                 </div>
                 <div className="container-fluid mar-top-bot-50" data-aos="fade-up" data-aos-delay="200">
                     <h4>Our Purpose</h4>
-                    <p>To encourage young minds who have the urge and knacks to innovate and impact the generations. To guide and nurture aspiring entrepreneurs through a series of mentoring sessions, boot camps, and getting a 360-degree approach to a complete cycle of Innovation.</p>
+                    <p>To encourage young minds who have the urge and knack to innovate and impact the generations. </p>
+                    <p>To guide and nurture aspiring entrepreneurs through a series of mentoring sessions, boot camps, and getting a 360-degree approach to a complete cycle of Innovation.</p>
                 </div>
                 <div className="container-fluid mar-top-bot-50" data-aos="fade-up" data-aos-delay="200">
                     <h4>Vision & Objective</h4>
-                    <p>Entrepreneurship Cell (E-Cell) is set up to foster an entrepreneurial mindset among the students of Karunya. The main motive of this dynamic organisation is to bring up young entrepreneurs and make Karunya, a start-up hub.</p>
+                    <p>The main motive of this dynamic organisation is to</p>
                     <Collapse in={val}>
-                        <p>Influence young minds to transform their innovative ideas into startups.</p>
-                        <p>Inculcate the entrepreneurial mindset among all the students of Karunya.</p>
-                        <p>Create a mindset among the students such that if a student has a startup idea, the next thing they do is to reach out to E-Cell Karunya.</p>
-                        <p>Promote women entrepreneurship in Karunya.</p>
-                        <p>Increase the ranking of campus with innovations and startups.</p>
+                        <p>★ Influence young minds to transform their innovative ideas into startups.</p>
+                        <p>★ Inculcate the entrepreneurial mindset among all the students of Karunya.</p>
+                        <p>★ Promote women entrepreneurship in Karunya.</p>
+                        <p>★ Make Karunya a startup hub.</p>
                     </Collapse>
                     <a href="##" onClick = {()=> {
                         if(val){
